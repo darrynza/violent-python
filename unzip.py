50acc4f516735f60f55711bce659f78a9e3d72e2
Justin R. Andrusk <jandrusk@gmail.com>
2014-02-15 11:46:05 -0500

    unzip.py - added optparse module options for passing zip file and dictionary on cmd line.

--- unzip.py
+++ unzip.py
@@ -1,17 +1,30 @@
 import zipfile
+import optparse
 from threading import Thread
 
 def extractFile(zFile, password):
 	try:
 		zFile.extractall(pwd=password)
-		print "Password cracked. Exiting.."
-		return password
+		print '[+] Found password ' + password + '\n'
 	except:
-		return
+		pass
 
 def main():
-	zFile =zipfile.ZipFile('all.zip')
-	passFile = open('dict.txt')
+	parser = optparse.OptionParser("usage: unzip.py "+\
+		"-f <zipfile> -d <dictionary>")
+	parser.add_option('-f', dest='zname', type='string',\
+		help='specify zip file')
+	parser.add_option('-d', dest='dname', type='string',\
+		help='specify dictionary file')
+	(options, args) = parser.parse_args()
+	if (options.zname == None):
+		print parser.usage
+		exit(0)
+	else:
+		zname = options.zname
+		dname = options.dname
+	zFile = zipfile.ZipFile(zname)
+	passFile = open(dname)
 	for line in passFile.readlines():
 		password = line.strip('\n')
 		t = Thread(target=extractFile, args=(zFile, password))
