6900e92a9ddf27d65699c944b779d143ea9a2293
Justin R. Andrusk <jandrusk@gmail.com>
2015-04-18 21:19:26 -0400

    new file:   good-proxies.txt 	new file:   viewPage.py

---
+++ viewPage.py
@@ -0,0 +1,9 @@
+import mechanize
+
+def viewPage(url):
+    browser = mechanize.Browser()
+    page = browser.open(url)
+    source_code = page.read()
+    print source_code
+
+viewPage('http://rmccurdy.com/scripts/proxy/good.txt')
